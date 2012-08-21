f9340c48c1d33448c0914b4687c26d6d7d369bca
Bryan Saunders <bsaunder@redhat.com>
2012-08-21 11:33:20 -0400

    Added Test Suite

---
+++ src/test/java/cs414/a1/btsaunde/TestAll.java
@@ -0,0 +1,17 @@
+package cs414.a1.btsaunde;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test Suite to Run All Tests.
+ * 
+ * @author Bryan Saunders <btsaunde@gmail.com>
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ CoordinateTest.class, WorldTest.class })
+public class TestAll {
+
+}
